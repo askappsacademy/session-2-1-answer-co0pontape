1526fb5882d4dfd952679b18e258a68f1c1f748f
annamalaigokkul <70380695+annamalaigokkul@users.noreply.github.com>
2020-09-24 17:17:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/********************
+
+In this exercise, we will be creating a function that takes in two numbers and returns the sum
+
+Step 1. Create and declare a function,
+that takes in two parameters
+
+Step 2. In the function body, add both
+numbers together and return the sum
+
+Step 3. Call the function
+
+For extra credit, create a conditional that executes a message if the sum is over 10 or less than 100 
+
+********************/
+
+// Your Solution:
+function Addition(value1, value2) {
+    var result = value1 + value2;
+    return result;
+};
+
+console.log(Addition(1,14));
